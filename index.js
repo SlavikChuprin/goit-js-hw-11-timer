22bbfab4037c337a722cf48b5d6c6897cf3735a3
SlavikChuprin <chuprin.00@yandex.ru>
2021-09-01 23:31:52 +0300

    fixed plugin
    добавил в плагин создание по id

--- index.js
+++ index.js
@@ -7,44 +7,41 @@ class CountdownTimer {
         this.dateNow = Date.now();
         this.time = this.targetDate.getTime() - this.dateNow;
         this.refs = {
-        valueDays: document.querySelector('.value[data-value="days"]'),
-        valueHours: document.querySelector('.value[data-value="hours"]'),
-        valueMins: document.querySelector('.value[data-value="mins"]'),
-        valueSecs: document.querySelector('.value[data-value="secs"]'),
+        valueDays: document.querySelector(`${this.selector} .value[data-value="days"]`),
+        valueHours: document.querySelector(`${this.selector} .value[data-value="hours"]`),
+        valueMins: document.querySelector(`${this.selector} .value[data-value="mins"]`),
+        valueSecs: document.querySelector(`${this.selector} .value[data-value="secs"]`),
         };
     }
     CountdownTimerStart() {
-       const intervalId = setInterval(() => {
-   
+      
+    const intervalId = setInterval(() => {
+        
          if (this.time !== 1000) {
-        const days = Math.floor(this.time / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const mins = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
-        const secs = Math.floor((this.time % (1000 * 60)) / 1000);
+            const days = Math.floor(this.time / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const mins = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
+            const secs = Math.floor((this.time % (1000 * 60)) / 1000);
 
-        this.time -= 1000;
-        this.refs.valueDays.textContent = this.padStart(days);
-        this.refs.valueHours.textContent = this.padStart(hours);
-        this.refs.valueMins.textContent = this.padStart(mins);
-        this.refs.valueSecs.textContent = this.padStart(secs);
+            this.time -= 1000;
+            this.refs.valueDays.textContent = this.padStart(days);
+            this.refs.valueHours.textContent = this.padStart(hours);
+            this.refs.valueMins.textContent = this.padStart(mins);
+            this.refs.valueSecs.textContent = this.padStart(secs);
 
-        return;
-    };
+            return;
+        };
     clearInterval(intervalId);
 }, 1000);
     }
-padStart(a) {
+    padStart(a) {
     return String(a).padStart(2, "0");
 };
 }
 
 const countDown = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Sep 1, 2021'),
+  targetDate: new Date('Oct 2, 2021'),
 });
-console.log(`countDown`, countDown)
-
 
 countDown.CountdownTimerStart();
-
-
