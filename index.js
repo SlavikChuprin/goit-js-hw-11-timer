d18a54bb5b1d60d44edf942aefb5a6179a9ead88
SlavikChuprin <chuprin.00@yandex.ru>
2021-09-01 23:38:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class CountdownTimer {
         };
     }
     CountdownTimerStart() {
+    console.log('targetDate',  this.targetDate);
       
     const intervalId = setInterval(() => {
         
